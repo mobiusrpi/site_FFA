0f7ed540d16f66cfbdba37d1dfb1dbc9f8eedc9c
unknown <jtremblet@gmail.com>
2025-04-30 16:47:32 +0200

    Command upload competitor from FFA spreadsheet

--- src/Command/ImportCompetitorsCommand.php
+++ src/Command/ImportCompetitorsCommand.php
@@ -2,7 +2,13 @@
 
 namespace App\Command;
 
+use App\Entity\Crews;
+use DateTimeImmutable;
 use App\Entity\Competitors;
+use App\Entity\Enum\CRAList;
+use App\Entity\Enum\Category;
+use App\Entity\Enum\Polosize;
+use App\Entity\Enum\SpeedList;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\CompetitorsRepository;
 use Symfony\Component\Serializer\Serializer;
@@ -49,14 +55,15 @@ class ImportCompetitorsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->uploadCompetitors();
+//         $this->uploadPilots();
+       $this->uploadNavigators();
 
         return Command::SUCCESS;
     }
 
     private function getDataFromFile(): array
     {
-        $file = $this->dataDirectory . 'IRRA-2025-2.csv';
+        $file = $this->dataDirectory . 'IRRA-2025-1.csv';
         $fileExtension = pathinfo($file,PATHINFO_EXTENSION);
 
         $normalizers = [new ObjectNormalizer];
@@ -70,13 +77,10 @@ class ImportCompetitorsCommand extends Command
         $fileString = file_get_contents($file);
         $data = $serializer->decode($fileString,$fileExtension);
         
-//       if (array_key_exist('results',$data)) {
-//            return $data['result'];
-//       }
         return $data;
     }
 
-    private function uploadCompetitors(): void
+    private function uploadPilots(): void
     {
         $competitorCreated = 0;
         foreach($this->getdataFromFile() as $row){          
@@ -85,23 +89,82 @@ class ImportCompetitorsCommand extends Command
                     'email' => $row['Adresse e-mail'],
                 ] );
 
-                if  (!$competitor) {
+                if  (!$competitor) {                          
                     $competitor = new Competitors;
+                    $pos = strrpos($row['Taille_polo_pilote'],'X',0);
+                    if (!empty($pos) and ($pos > 0)){
+                        $polo = $pos + 1 . 'XL';
+                    }
+                    else{
+                        $polo = $row['Taille_polo_pilote'];
+                    };
                     $competitor->setEmail($row['Adresse e-mail'])
                                 ->setLastname($row['Nom pilote'])
                                 ->setFirstname($row['Prénom_pilote'])
-                                ->setFfaLicence($row['N°_licence FFA_pilote']);
+                                ->setFfaLicence($row['N°_licence FFA_pilote'])
+                                ->setFlyingclub($row['Aéro-club_prise_de_licence_pilote'])
+                                ->setPhone($row['Téléphone_portable_pilote'])
+                                ->setCommittee(CRAList::from($row['CRA_Licence_pilote']))                              
+                                ->setPoloSize(Polosize::from($polo));
+                  
+                    $this->entityManager->persist($competitor);
+                    $competitorCreated++;
+                }
+            } 
+        }                  
+        $this->entityManager->flush();
+
+        if ($competitorCreated > 0 ){           
+            $string1 = '{$competitorCreated} compétiteurs d\'ajoutés';
+        }
+        else {
+            $string1 = '';
+        }
+
+        $this->io->success($string1);
+    }
+
+    private function uploadNavigators(): void
+    {
+        $competitorCreated = 0;
+        foreach($this->getdataFromFile() as $row){          
+            if (array_key_exists('adresse_mail_navigateur',$row) && !empty($row['adresse_mail_navigateur'])) {
+                $competitor = $this->competitorRepository->findOneBy([
+                    'email' => $row['adresse_mail_navigateur'],
+                ] );
+
+                if  (!$competitor) {
+                    $pos = strrpos($row['Taille_polo_navigateur'],'X',0);
+                    if (!empty($pos) and ($pos > 0)){
+                        $polo = $pos + 1 . 'XL';
+                    }
+                    else{
+                        $polo = $row['Taille_polo_navigateur'];
+                    };
+                    $competitor = new Competitors;
+                    $competitor->setEmail($row['adresse_mail_navigateur'])
+                                ->setLastname($row['Nom_navigateur'])
+                                ->setFirstname($row['Prénom_navigateur'])
+                                ->setFfaLicence($row['N°_licence FFA_navigateur'])
+                                ->setFlyingclub($row['Aéro-club_prise_de_licence_navigateur'])
+                                ->setPhone($row['Téléphone_portable_navigateur'])
+                                ->setCommittee(CRAList::from($row['CRA_Licence_navigateur']))
+                                ->setPoloSize(Polosize::from($polo));
+                  
                     $this->entityManager->persist($competitor);
                     $competitorCreated++;
                 } 
             } 
-        }         
+        }                  
         $this->entityManager->flush();
 
         if ($competitorCreated > 0 ){           
-            $string = '{$userCreated} compétiteurs d\'ajoutés';
+            $string2 = '{$competitorCreated} compétiteurs d\'ajoutés';
+        }
+        else {
+            $string2 = '';
         }
 
-        $this->io->success($string);
+        $this->io->success($string2);
     }
 }
