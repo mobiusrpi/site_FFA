10e47cb500836888567cc3ecef56593ad9d34505
mobiusrpi <jtremblet@gmail.com>
2025-08-26 11:29:12 +0200

    sendMail service test

--- src/Service/SendMailService.php
+++ src/Service/SendMailService.php
@@ -2,20 +2,23 @@
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 
 class SendMailService
 {
-    private $mailer;
+    private $mailer;    
+    private $logger;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(MailerInterface $mailer, LoggerInterface $logger)
     {
-        $this->mailer = $mailer;
+        $this->mailer = $mailer;        
+        $this->logger = $logger; 
     }
 
     public function send(
-        string $from,
+        string $from,      
         string $to,
         string $subject,
         string $template,
@@ -28,7 +31,8 @@ class SendMailService
             ->subject($subject)
             ->htmlTemplate("emails/$template.html.twig")
             ->context($context);
-
+    dd($email::class);
+        $this->logger->info("Envoi email async à {$to}");
         $this->mailer->send($email);
     }
 }
