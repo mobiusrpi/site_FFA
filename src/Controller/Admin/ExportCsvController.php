f1f1a3d330c4e833bb3529f69256a00c24aeb99f
mobiusrpi <jtremblet@gmail.com>
2026-04-03 19:24:56 +0200

    Enhance filename generation in exportCrews method to handle special characters and improve formatting

--- src/Controller/Admin/ExportCsvController.php
+++ src/Controller/Admin/ExportCsvController.php
@@ -103,8 +103,23 @@ class ExportCsvController extends AbstractController
             throw $this->createNotFoundException('Aucun équipage trouvé.');
         }
         $competition = $crews[0]->getCompetition();
-        $competName = $competition->getName();
-        $filename = 'ExportCrews_' . preg_replace('/[^a-zA-Z0-9_-]/', '_', $competName) . '.csv';
+
+        $competName = $competition->getName(); 
+
+        $map = [
+            'À'=>'A','Á'=>'A','Â'=>'A','Ã'=>'A','Ä'=>'A','Å'=>'A','Æ'=>'AE','Ç'=>'C','È'=>'E','É'=>'E','Ê'=>'E','Ë'=>'E',
+            'Ì'=>'I','Í'=>'I','Î'=>'I','Ï'=>'I','Ð'=>'D','Ñ'=>'N','Ò'=>'O','Ó'=>'O','Ô'=>'O','Õ'=>'O','Ö'=>'O','Ø'=>'O',
+            'Ù'=>'U','Ú'=>'U','Û'=>'U','Ü'=>'U','Ý'=>'Y','Þ'=>'TH','ß'=>'ss',
+            'à'=>'a','á'=>'a','â'=>'a','ã'=>'a','ä'=>'a','å'=>'a','æ'=>'ae','ç'=>'c','è'=>'e','é'=>'e','ê'=>'e','ë'=>'e',
+            'ì'=>'i','í'=>'i','î'=>'i','ï'=>'i','ð'=>'d','ñ'=>'n','ò'=>'o','ó'=>'o','ô'=>'o','õ'=>'o','ö'=>'o','ø'=>'o',
+            'ù'=>'u','ú'=>'u','û'=>'u','ü'=>'u','ý'=>'y','þ'=>'th','ÿ'=>'y'
+        ];
+        $competName = strtr($competName, $map);
+        $competName = preg_replace('/[^a-zA-Z0-9_-]+/', '_', $competName);
+        $competName = trim($competName, '_-');
+        $competName = preg_replace('/[_-]{2,}/', '_', $competName);
+
+        $filename = 'ExportCrews_' . $competName . '.csv';
 
         $competitionAccommodations = [];
         foreach ($competition->getCompetitionAccommodation() as $compAcc) {
