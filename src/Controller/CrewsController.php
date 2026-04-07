f9cee6cd682b81bda18bb4f3a4419727e09ebe2c
mobiusrpi <jtremblet@gmail.com>
2026-04-07 15:30:08 +0200

    test1 -

--- src/Controller/CrewsController.php
+++ src/Controller/CrewsController.php
@@ -408,10 +408,7 @@ public function registration_listt(
     {       
         /** @var Users|null $user */
         $user = $security->getUser();
-dump($security->getUser());
-dump($user);
-dump($security->getUser()?->getRoles());
-die();
+
         $competByUser = $repositoryCrew->getQueryRegistrationsCrews($user->getId());
 
         return $this->render('pages/crews/registrationCrewsList.html.twig', [
