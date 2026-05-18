8a115a52bc08a631d28dfaeafefebf3f0173973e
mobiusrpi <jtremblet@gmail.com>
2026-05-18 18:43:39 +0200

    listenner

--- src/EventListener/EasyAdminPrintCssListener.php
+++ src/EventListener/EasyAdminPrintCssListener.php
@@ -2,19 +2,44 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpFoundation\Response;
 
 class EasyAdminPrintCssListener
 {
     public function onKernelResponse(ResponseEvent $event)
     {
         $response = $event->getResponse();
+
+        // 🚨 NE JAMAIS TOUCHER AUX FICHIERS
+        if ($response instanceof BinaryFileResponse) {
+            return;
+        }
+
+        // uniquement HTML
+        if (!$response instanceof Response) {
+            return;
+        }
+
         $content = $response->getContent();
 
-        // injecte le CSS juste avant </head>
-        $css = '<link rel="stylesheet" href="/css/easyadmin-print.css">';
-        $content = str_replace('</head>', $css.'</head>', $content);
+        if (!is_string($content)) {
+            return;
+        }
 
-        $response->setContent($content);
+        // sécurité HTML uniquement
+        if (str_contains($content, '</head>')) {
+
+            $css = '<link rel="stylesheet" href="/css/easyadmin-print.css">';
+
+            $content = str_replace(
+                '</head>',
+                $css . '</head>',
+                $content
+            );
+
+            $response->setContent($content);
+        }
     }
 }
